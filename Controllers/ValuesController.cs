95928ac8be0de4975066a431529bc4d22372ad3e
Samuel-Lucas <samuel.moreira10@outlook.com>
2021-11-22 23:05:42 -0300

    WebApi com SqLite

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProAgil.WebApi.Data;
+using ProAgil.WebApi.Model;
+
+namespace ProAgil.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // api/[controller]
+    public class ValuesController : ControllerBase
+    {
+        public DataContext _context { get; }
+
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var result = await _context.Eventos.ToListAsync();
+
+                return Ok(result);
+            }
+            catch(Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de dados falhou");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                var result = await _context.Eventos.FirstOrDefaultAsync(x => x.EventoId == id);
+
+                return Ok(result);
+            }
+            catch(Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de dados falhou");
+            }
+        }
+    }
+}
